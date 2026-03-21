17270017fab67635ac958f5200790dc2aa0bf0ba
LucasMorgani <morganis1411@gmail.com>
2026-03-21 12:09:04 -0300

    fix: updating variables

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,19 +1,19 @@
 output "eks_cluster_endpoint" {
   description = "EKS cluster API server endpoint"
-  value       = aws_eks_cluster.eks.endpoint
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "eks_cluster_name" {
   description = "EKS cluster name"
-  value       = aws_eks_cluster.eks.name
+  value       = aws_eks_cluster.this.name
 }
 
 output "eks_cluster_security_group_id" {
   description = "EKS cluster security group ID"
-  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+  value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
 }
 
 output "cluster_authentic" {
   description = "EKS cluster certificate authority data"
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+  value = aws_eks_cluster.this.certificate_authority[0].data
 }

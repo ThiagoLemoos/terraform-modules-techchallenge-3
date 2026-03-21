7f18540c677d1c72b91837494f49c6b694c67830
LucasMorgani <morganis1411@gmail.com>
2026-03-21 11:54:35 -0300

    feat: add kubernetes module to create secrets from aws

--- environments/hml/providers.tf
+++ environments/hml/providers.tf
@@ -5,9 +5,31 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.95.0, < 6.0.0"
     }
+    kubernetes = {  #kubernetes
+      source  = "hashicorp/kubernetes" #kubernetes
+      version = "3.0.1" #kubernetes
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+#Kubernetes provider to exec a job
+provider "kubernetes" { 
+  host = module.eks.eks_cluster_endpoint 
+  cluster_ca_certificate = base64decode( 
+    module.eks.cluster_authentic
+  )
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name", var.eks_cluster_name,
+    ]
+  }
 }
--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,9 +1,19 @@
 output "eks_cluster_endpoint" {
   description = "EKS cluster API server endpoint"
-  value       = null
+  value       = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.eks.name
 }
 
 output "eks_cluster_security_group_id" {
   description = "EKS cluster security group ID"
-  value       = null
+  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_authentic" {
+  description = "EKS cluster certificate authority data"
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }

db3309204786a9d9168d6031163e5c514df8d85a
Thiago Lemos <thiago.lemos.ps@gmail.com>
2026-03-22 16:36:39 -0300

    Update S3 bucket name for backend configuration

--- environments/hml/backend.tf
+++ environments/hml/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-techchallenge-equipe7-teste"
+    bucket = "terraform-state-techchallenge-equipe7"
     key    = "techchallenge3/hml/terraform.tfstate"
     region = "us-east-1"
   }

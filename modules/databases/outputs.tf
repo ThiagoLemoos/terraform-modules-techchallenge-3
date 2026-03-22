a8adf0b4ea9eaacd8cb55288e0c2a0006922c3a2
LucasMorgani <morganis1411@gmail.com>
2026-03-22 16:43:49 -0300

    fix: updating variables

--- modules/databases/outputs.tf
+++ modules/databases/outputs.tf
@@ -21,5 +21,5 @@ output "rds_password" {
 
 output "elasticache_endpoint" {
   description = "ElastiCache/Redis endpoint"
-  value       = module.elasticache.elasticache_endpoint
+  value       = module.elasticache.cluster_endpoint[0]
 }
